3f2f51ce9474bdc9624a9609d88b36b5c92a10fb
marcosdima <marcosdimatteo.wgr@gmail.com>
2023-09-09 19:56:20 -0300

    Warning fix

--- src/entities/attributes/BaseAttributes.cs
+++ src/entities/attributes/BaseAttributes.cs
@@ -2,23 +2,28 @@ using System.Collections.Generic;
 using System;
 
 public class BaseAttributes {
-    public Dictionary<Attribute, int> attributes;
+    public Dictionary<Attribute, int>? attributes;
 
     public BaseAttributes() {
+        // Because of Error CS8618.
+        this.attributes = new Dictionary<Attribute, int>();
         this.SetAttributes();
     }
 
     private void SetAttributes() {
         this.attributes = new Dictionary<Attribute, int>();
+        // Gets the values of the Attribute enum.
         Attribute[] attributes = (Attribute[]) Enum.GetValues(typeof(Attribute));
         foreach (Attribute attr in attributes) {
             this.attributes.Add(attr, 0);
         }
     }
     private void ModifyAttribute(Attribute attribute, int magnitude) {
-        // If the key exists, increase the magnitude.
+        // If the key exists, adds the magnitude.
         if (this.attributes.ContainsKey(attribute)) 
-            attributes[attribute] = Math.Max(attributes[attribute] += magnitude, 0);
+            // The attributes must be positive or 0.
+            this.attributes[attribute] = Math.Max(this.attributes[attribute] += magnitude, 0);
+        // This never should happen.
         else Console.WriteLine("The attribute ", attribute, " doesn't exist...");
     }
     public void RiseAttribute(Attribute attribute) {
